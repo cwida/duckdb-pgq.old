15a95534c3ed853cce0ef2aeea874f9e5c5eb234
tav-singh <tavneet12113@iiitd.ac.in>
2021-04-13 14:38:33 +0200

    Saving the property graph to catalog and getting an error on repeating the test. Commenting serialisation code, removing initialisation from PropertyGraphSchemaCatalog Entry and using function overloading instead.

--- src/catalog/catalog_entry/property_graph_catalog_entry.cpp
+++ src/catalog/catalog_entry/property_graph_catalog_entry.cpp
@@ -11,45 +11,40 @@
 
 namespace duckdb {
 
-void PropertyGraphCatalogEntry::Initialize(CreatePropertyGraphInfo *info) {
-	// query = move(info->query);
-	this->name = info->name;
-	// this->vertex_tables = info->vertex_tables;
-	// this->edge_tables = info->edge_tables;
-	// this->aliases = info->aliases;
-	// this->types = info->types;
-	// this->temporary = info->temporary;
-	// this->sql = info->sql;
-	// this->internal = info->internal;
-}
+// void PropertyGraphCatalogEntry::Initialize(CreatePropertyGraphInfo *info) {
+// 	this->name = info->name;
+// 	this->vertex_tables = info->vertex_tables;
+// 	this->edge_tables = info->edge_tables;
+
+// }
 
 PropertyGraphCatalogEntry::PropertyGraphCatalogEntry(Catalog *catalog, SchemaCatalogEntry *schema,
                                                      CreatePropertyGraphInfo *info)
     : StandardEntry(CatalogType::PROPERTY_GRAPH_ENTRY, schema, catalog, info->name) {
-	Initialize(info);
+	// Initialize(info);
 }
 
-void PropertyGraphCatalogEntry::Serialize(Serializer &serializer) {
-	D_ASSERT(!internal);
-	serializer.WriteString(schema->name);
-	serializer.WriteString(name);
-	D_ASSERT(vertex_tables.size() <= NumericLimits<uint32_t>::Maximum());
-	serializer.Write<uint32_t>((uint32_t)vertex_tables.size());
-}
+// void PropertyGraphCatalogEntry::Serialize(Serializer &serializer) {
+// 	D_ASSERT(!internal);
+// 	serializer.WriteString(schema->name);
+// 	serializer.WriteString(name);
+// 	D_ASSERT(vertex_tables.size() <= NumericLimits<uint32_t>::Maximum());
+// 	serializer.Write<uint32_t>((uint32_t)vertex_tables.size());
+// }
 
-unique_ptr<CreatePropertyGraphInfo> PropertyGraphCatalogEntry::Deserialize(Deserializer &source) {
-	auto info = make_unique<CreatePropertyGraphInfo>();
-	info->schema = source.Read<string>();
-	auto vertex_table_count = source.Read<uint32_t>();
-	for (uint32_t i = 0; i < vertex_table_count; i++) {
-		info->vertex_tables.push_back(PropertyGraphTable::Deserialize(source));
-	}
-	auto edge_table_count = source.Read<uint32_t>();
-	for (uint32_t i = 0; i < edge_table_count; i++) {
-		info->edge_tables.push_back(PropertyGraphTable::Deserialize(source));
-	}
-	return info;
-}
+// unique_ptr<CreatePropertyGraphInfo> PropertyGraphCatalogEntry::Deserialize(Deserializer &source) {
+// 	auto info = make_unique<CreatePropertyGraphInfo>();
+// 	info->schema = source.Read<string>();
+// 	auto vertex_table_count = source.Read<uint32_t>();
+// 	for (uint32_t i = 0; i < vertex_table_count; i++) {
+// 		info->vertex_tables.push_back(PropertyGraphTable::Deserialize(source));
+// 	}
+// 	auto edge_table_count = source.Read<uint32_t>();
+// 	for (uint32_t i = 0; i < edge_table_count; i++) {
+// 		info->edge_tables.push_back(PropertyGraphTable::Deserialize(source));
+// 	}
+// 	return info;
+// }
 
 // string PropertyGraphCatalogEntry::ToSQL() {
 // 	if (sql.size() == 0) {
--- src/include/duckdb/catalog/catalog_entry/property_graph_catalog_entry.hpp
+++ src/include/duckdb/catalog/catalog_entry/property_graph_catalog_entry.hpp
@@ -11,42 +11,32 @@
 #include "duckdb/catalog/standard_entry.hpp"
 #include "duckdb/common/unordered_map.hpp"
 #include "duckdb/parser/property_graph_table.hpp"
-#include "duckdb/planner/bound_constraint.hpp"
 #include "duckdb/planner/expression.hpp"
 
 namespace duckdb {
 
-// class ColumnStatistics;
-// class DataTable;
 struct CreatePropertyGraphInfo;
-// struct BoundCreateTableInfo;
 
 //! A table catalog entry
 class PropertyGraphCatalogEntry : public StandardEntry {
 public:
 	//! Create a real PropertyGraphCatalogEntry and initialize storage for it
-	// ViewCatalogEntry(Catalog *catalog, SchemaCatalogEntry *schema, CreateViewInfo *info);
-
 	PropertyGraphCatalogEntry(Catalog *catalog, SchemaCatalogEntry *schema, CreatePropertyGraphInfo *info);
-	//! A reference to the underlying storage unit used for this table
-	// std::shared_ptr<DataTable> storage;
 
-	// vector<PropertyGraphTable> vertex_tables;
-	// vector<PropertyGraphTable> edge_tables;
 	string name;
 	vector<unique_ptr<PropertyGraphTable>> vertex_tables;
 	vector<unique_ptr<PropertyGraphTable>> edge_tables;
-	// vector<unique_ptr<BaseTableRef>> table_ref_list;
 
 public:
 	//! Serialize the meta information of the PropertyGraphCatalogEntry a serializer
-	virtual void Serialize(Serializer &serializer);
-	//! Deserializes to a CreatePropertyGraphInfo
-	static unique_ptr<CreatePropertyGraphInfo> Deserialize(Deserializer &source);
+	// virtual void Serialize(Serializer &serializer);
+	// //! Deserializes to a CreatePropertyGraphInfo
+	// static unique_ptr<CreatePropertyGraphInfo> Deserialize(Deserializer &source);
 
 	unique_ptr<CatalogEntry> Copy(ClientContext &context) override;
 
-private:
-	void Initialize(CreatePropertyGraphInfo *info);
+	// private:
+	// 	void Initialize(CreatePropertyGraphInfo *info);
 };
+
 } // namespace duckdb
