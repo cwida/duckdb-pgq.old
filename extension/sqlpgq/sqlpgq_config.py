3a5f1cb2779d7088c66a80d7f68c3d3171afa928
daniel <dljtw98@gmail.com>
2022-05-17 17:27:19 +0200

    Added sqlpgq_config.py

---
+++ extension/sqlpgq/sqlpgq_config.py
@@ -0,0 +1,9 @@
+import os
+
+# list all include directories
+include_directories = [os.path.sep.join(x.split('/')) for x in ['extension/sqlpgq/include']]
+# source files
+source_files = [os.path.sep.join(x.split('/')) for x in
+                ['extension/sqlpgq/sqlpgq-extension.cpp', 'extension/sqlpgq/sqlpgq_functions/sqlpgq_cheapest_path.cpp',
+                 'extension/sqlpgq/sqlpgq_functions/sqlpgq_csr_creation.cpp', 'extension/sqlpgq/sqlpgq_functions/sqlpgq_reachability.cpp',
+                 'extension/sqlpgq/sqlpgq_functions/sqlpgq_shortest_path.cpp']]
